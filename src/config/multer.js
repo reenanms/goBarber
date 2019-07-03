4851d0a480f410102244f1976c2a99dd49aad26d
reenanms <reenanms@gmail.com>
2019-07-03 00:22:31 -0300

    adding the providers route

--- src/config/multer.js
+++ src/config/multer.js
@@ -8,7 +8,7 @@ export default {
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
         if (err) return cb(err);
-        return cb(null, res.toString('hex') + file.originalname);
+        return cb(null, res.toString('hex') + extname(file.originalname));
       });
     },
   }),
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,5 +18,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
