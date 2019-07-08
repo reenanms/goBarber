141a76921276cef92baf668dc8e6506b485fa8b5
reenanms <reenanms@gmail.com>
2019-07-07 23:24:18 -0300

    adding providers notifications controls with mongodb

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,8 +19,12 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
