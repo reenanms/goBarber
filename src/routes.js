770c340a475e162fe888421a25bfc2d51f72c1c2
reenanms <reenanms@gmail.com>
2019-07-12 09:57:11 -0300

    created the avaliable route to provider, the  shows a provider's day schedule

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,6 +25,7 @@ routes.put('/notifications/:id', NotificationController.update);
 
 routes.get('/appointments', AppointmentController.index);
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:id/available', AvailableController.index);
 routes.get('/schedule', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
 
